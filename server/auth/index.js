990a241c3cfdc7cef31656cc93a9733e924d4d47
ThibaultYVD <thibaultyvard01@gmail.com>
2024-04-29 11:19:11 +0200

    reorganize project folders

---
+++ server/auth/index.js
@@ -0,0 +1,42 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const cookieSession = require("cookie-session")
+app.use(cors())
+require('dotenv/config')
+const db = require("./model");
+
+// db.sequelize.sync({ force: true }).then(() => {
+//     console.log('Drop and Resync Db');
+//     initial();
+// });
+
+db.sequelize.sync()
+
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+    cookieSession({
+        name: "website-session",
+        keys: [process.env.SECRET_KEY],
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000 // 24h
+    })
+);
+
+app.get("/home", (req, res) => {
+    res.json({ message: "API Registration/login" });
+});
+
+
+// Routes
+require('./routes/auth')(app)
+require('./routes/user')(app);
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
