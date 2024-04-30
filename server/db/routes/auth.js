544d8f8bb200a376e031d9bd16239bcbdf2a0a4c
ThibaultYVD <thibaultyvard01@gmail.com>
2024-04-30 16:48:40 +0200

    test

--- server/db/routes/auth.js
+++ server/db/routes/auth.js
@@ -1,44 +1,98 @@
 const express = require('express')
-
 const router = express.Router()
-const axios = require('axios')
+const db = require('../model/Models');
+const jwt = require("jsonwebtoken");
 
-
-router.post('/signin', async (req, res) => {
+router.get('/', async (req, res) => {
     try {
-        const email = req.body.email
-        const password = req.body.password
-
-        const response = await axios.post('http://localhost:20242/api/auth/signin', {
-            email,
-            password
-        });
-
-        req.session.token = response.data.token
-
-        res.json(req.session.token);
-    } catch (error) {
-        console.log(error)
+        const groups = await db.Group.findAll();
+        res.status(200).json(groups);
+    } catch (err) {
+        console.error('Error dans récupération des groupes :', err);
+        res.status(500).json({ error: 'Error dans récupération des groupes' });
     }
+});
 
-})
 
-router.post('/signout', async (req, res) => {
 
+
+router.get('/:groupId', async (req, res) => {
     try {
-        await axios.post('http://localhost:20242/api/auth/signout');
-        req.session.destroy(err => {
-            if (err) {
-                console.error('Erreur lors de la destruction de la session :', err);
-                return res.status(500).json({ error: 'Erreur lors de la destruction de la session' });
-            } else {
-                return res.status(201).json({ message: "Session détruite avec succès" })
-            }
-        });
+        const group = await db.Group.findByPk(req.params.groupId)
+        if (group === null) {
+            res.status(404).json({ error: 'Group non trouvé.' });
+        } else {
+            res.status(200).json(group)
+        }
+
     } catch (error) {
-        console.log(error)
+        console.error(`Error dans récupération du groupe ${req.params.groupId} :`, err);
+        res.status(500).json({ error: 'Error dans récupération du groupe' });
     }
 })
 
+router.post('/', async (req, res) => {
+    const { group_name, creator_id } = req.body;
+
+    try {
+        const createdGroup = await db.Group.create({
+            group_name: group_name,
+            creation_date: new Date(),
+            creator_id: creator_id
+        });
+
+        res.status(201).json(createdGroup);
+    } catch (error) {
+        console.error(`Error dans l'insertion du groupe :`, error);
+        res.status(500).json({ error: "Erreur dans l'insertion du groupe." });
+    }
+})
+
+router.delete('/:groupId', async (req, res) => {
+    try {
+        const existingGroup = await db.Group.findByPk(req.params.groupId)
+
+        if (!existingGroup) {
+            return res.status(404).json({ error: 'Groupe introuvable' })
+        }
+
+        const removeGroup = await db.Group.destroy({
+            where: { group_id: req.params.groupId }
+        })
+        res.status(204).json(removeGroup)
+    } catch (error) {
+        console.error(`Error dans suppression du groupe ${req.params.groupId} :`, error);
+        res.status(500).json({ error: 'Error dans suppression du groupe' });
+    }
+})
+
+router.patch('/:groupId', async (req, res) => {
+
+    const { group_name, creator_id } = req.body
+
+    try {
+        const existingGroup = await db.Group.findByPk(req.params.groupId)
+
+        if (!existingGroup) {
+            return res.status(404).json({ error: 'Groupe introuvable' })
+        }
+
+        const patchedGroup = await db.Group.update
+            ({
+                group_name: group_name,
+                creator_id: creator_id
+            },
+                {
+                    where: { group_id: req.params.groupId },
+                })
+
+        res.status(200).json(patchedGroup)
+
+    } catch (error) {
+        console.error(`Error dans maj du groupe ${req.params.groupId} :`, error);
+        res.status(500).json({ error: 'Error dans maj du groupe' });
+    }
+
+})
 
 module.exports = router
