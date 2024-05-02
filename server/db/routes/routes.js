18b905bcaf1d183b1123e937eea0658718bc72e1
ThibaultYVD <thibaultyvard01@gmail.com>
2024-05-02 11:37:57 +0200

    finished groups

--- server/db/routes/routes.js
+++ server/db/routes/routes.js
@@ -1,17 +1,19 @@
-async function routes (app) {
+async function routes(app) {
     app.get("/", (req, res) => {
         res.json({ message: "DB Server" });
     });
 
+    /* ROUTES ADMIN */
     const groupsAdmin = require('./admin/groups')
-    app.use('/admin/', groupsAdmin)
+    app.use('/admin/groups/', groupsAdmin)
 
+    
+    /* ROUTES USERS */
+    const groupsRoute = require('./user/groups')
+    app.use('/groups/', groupsRoute)
 
     const productsRoute = require('./products')
     app.use('/products/', productsRoute)
-
-    const groupsRoute = require('./groups')
-    app.use('/groups/', groupsRoute)
 
     const listsRoute = require('./lists')
     app.use('/lists/', listsRoute)
