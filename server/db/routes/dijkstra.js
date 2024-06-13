2d9868651f646314e9c99c4d4da4e2a4e85f8874
Thibault Yvard <thibaultyvd@vmi998189.contaboserver.net>
2024-06-13 11:43:20 +0200

    done

--- server/db/routes/dijkstra.js
+++ server/db/routes/dijkstra.js
@@ -4,6 +4,8 @@ const db = require('../model/Models');
 
 
 function dijkstra(graph, source) {
+
+
     const distances = {};
     const pq = [];
     let closestPoint = null;
@@ -53,16 +55,21 @@ function dijkstra(graph, source) {
 
 
 function euclideanDistance(point1, point2) {
-    return Math.sqrt(
-        Math.pow(point1.x - point2.x, 2) + Math.pow(point1.y - point2.y, 2)
+    let sqrt = Math.sqrt(
+        Math.pow(point1.location_x - point2.location_x, 2) + Math.pow(point1.location_y - point2.location_y, 2)
     );
+
+    return sqrt
 }
 
 // Construire un graph avec les coordonnées des rayons
 function buildGraph(points) {
+
     const graph = {};
 
     points.forEach((pointI, i) => {
+
+        
         graph[pointI.shelf_id] = {};
 
         points.forEach((pointJ, j) => {
@@ -79,28 +86,25 @@ function buildGraph(points) {
 
 
 router.get('/', async (req, res) => {
-
     try {
+        const user_id = 30;
+        const list_id = 1;
 
-        const user_id = 30
-        const list_id = 1
-
-
-        const checkIfExist = await db.sequelize.query(`SELECT * FROM list_routes WHERE list_id = :list_id`,
+        const checkIfExist = await db.sequelize.query(
+            `SELECT * FROM list_routes WHERE list_id = :list_id`,
             {
                 replacements: {
                     list_id: list_id,
-                }, type: db.sequelize.QueryTypes.SELECT,
-            });
+                },
+                type: db.sequelize.QueryTypes.SELECT,
+            }
+        );
 
-        let route
+        if (checkIfExist.length !== 0) {
+            return res.status(201).json(checkIfExist[0].route);
+        }
 
-        if(checkIfExist.length != 0) return res.status(201).json(checkIfExist[0].route)
-                    
-
-        
-    
-            let sql = 
+        let sql =
             `SELECT DISTINCT 
                 s.shelf_id, s.shelf_name, s.location_x, s.location_y
     
@@ -113,109 +117,81 @@ router.get('/', async (req, res) => {
             INNER JOIN group_members gm ON gm.group_id = g.group_id
             
             WHERE l.list_id = :list_id
-            AND gm.user_id = :user_id`
+            AND gm.user_id = :user_id`;
+
+        const shelvesData = await db.sequelize.query(sql, {
+            replacements: {
+                list_id: list_id,
+                user_id: user_id,
+            },
+            type: db.sequelize.QueryTypes.SELECT,
+        });
+
+        let source = '11';
+        let route = [];
+
+        route.push({
+            start: {
+                shelf_id: 0,
+                shelf_name: "Entrée",
+            },
+        });
+
+        while (shelvesData.length > 1) {
+            let { closestPoint } = dijkstra(buildGraph(shelvesData), source);
+
+            sql =
+                `SELECT DISTINCT 
+                    pl.product_id, p.product_name, p.price, pl.quantity
     
-            const shelvesData = await db.sequelize.query(sql,
-            {
+                FROM products_list pl
+    
+                INNER JOIN products p ON pl.product_id = p.product_id
+                INNER JOIN shelves s ON s.shelf_id = p.shelf_id
+            
+                WHERE s.shelf_id = :shelf_id
+                AND pl.list_id = :list_id`;
+
+            const products = await db.sequelize.query(sql, {
                 replacements: {
                     list_id: list_id,
-                    user_id: user_id
-                }, type: db.sequelize.QueryTypes.SELECT,
+                    shelf_id: closestPoint,
+                },
+                type: db.sequelize.QueryTypes.SELECT,
             });
 
-            console.log(shelvesData)
+            // Find the shelf_name corresponding to closestPoint
+            const closestShelf = shelvesData.find(
+                (shelf) => parseInt(shelf.shelf_id) === parseInt(closestPoint)
+            );
 
-    
-            const points = []
-    
-            for (let i = 0; i < shelvesData.length; i++){
-    
-                points.push({
-                    shelf_id: shelvesData[i].shelf_id, 
-                    shelf_name: shelvesData[i].shelf_name,
-                    x:shelvesData[i].location_x, 
-                    y:shelvesData[i].location_y
-                })
-            }
-
-            console.log(points)
-    
-            let source = '11';
-            route = []
-    
             route.push({
-                start: {
-                    shelf_id:0,
-                    shelf_name:"Entrée"
-                }                 
+                nextStop: {
+                    shelf_id: parseInt(closestPoint),
+                    shelf_name: closestShelf.shelf_name,
+                    products: products,
+                },
             });
-    
-            while(points.length > 1){
-                let { closestPoint } = dijkstra(buildGraph(points), source);
-        
-                sql = 
-                    `SELECT DISTINCT 
-                        s.shelf_name
-            
-                    FROM shelves s 
-        
-                    WHERE s.shelf_id = :shelf_id`
-        
-                const toShelveName = await db.sequelize.query(sql,
-                {
-                    replacements: {
-                        shelf_id: closestPoint,
-                    }, type: db.sequelize.QueryTypes.SELECT,
-                });
-    
-                sql = 
-                `SELECT DISTINCT 
-                pl.product_id, p.product_name, p.price, pl.quantity
-    
-            FROM products_list pl
-    
-            INNER JOIN products p ON pl.product_id = p.product_id
-            INNER JOIN shelves s ON s.shelf_id = p.shelf_id
-            
-            WHERE s.shelf_id = :shelf_id
-            AND pl.list_id = :list_id`
-    
-                const products = await db.sequelize.query(sql,
-                    {
-                        replacements: {
-                            list_id: list_id,
-                            shelf_id: closestPoint,
-                        }, type: db.sequelize.QueryTypes.SELECT,
-                    });
-    
-                route.push({
-                        nextStop: {
-                            shelf_id:parseInt(closestPoint),
-                            shelf_name:toShelveName[0].shelf_name,
-                            products:products
-                        }                 
-                    });
-    
-                const sourceIndex = points.findIndex(point => parseInt(point.shelf_id) === parseInt(source));
-                points.splice(sourceIndex, 1)
-                source = closestPoint;
-            }
-    
-            route.push(
-                { 
-                    end: {
-                        shelf_id:99,
-                        shelf_name:"Sortie"
-                    }       
-                });
 
+            const sourceIndex = shelvesData.findIndex(
+                (point) => parseInt(point.shelf_id) === parseInt(source)
+            );
+            shelvesData.splice(sourceIndex, 1);
+            source = closestPoint;
+        }
 
-        res.json(route)
-        
- } catch (error) {
-        console.log(error)
+        route.push({
+            end: {
+                shelf_id: 99,
+                shelf_name: "Sortie",
+            },
+        });
+
+        res.json(route);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-
-})
+});
 
 module.exports = router
