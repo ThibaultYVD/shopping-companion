81a0f0e634749e33387238fd22d73b1b8e5225c3
Thibault53 <yvard.thibault1@gmail.com>
2024-06-19 12:12:51 +0200

    Vue Initial commit

---
+++ client/src/routes/index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import Login from '../views/Login.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: Contact
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
