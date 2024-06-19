e1f726244bc2a500a5bc2edcc77d8aa0256879f8
ThibaultYVD <thibaultyvard01@gmail.com>
2024-06-19 12:12:51 +0200

    Vue Initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    
+  </main>
+</template>
+
+<style scoped>
+
+</style>
