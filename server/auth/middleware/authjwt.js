fe376802d77014602ee8fb01443b225f3fc26671
ThibaultYVD <thibaultyvard01@gmail.com>
2024-06-21 16:24:24 +0200

    suppression fichier config auth

--- server/auth/middleware/authjwt.js
+++ server/auth/middleware/authjwt.js
@@ -1,5 +1,4 @@
 const jwt = require("jsonwebtoken");
-const config = require("../config/auth.js");
 const db = require("../model");
 const User = db.User;
 
@@ -12,7 +11,7 @@ verifyToken = (req, res, next) => {
         });
     }
 
-    jwt.verify(token, config.secret, (err, decoded) => {
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
         if (err) {
             return res.status(401).send({
                 message: "Vous n'avez pas d'autorisation.",
