59826de8ffb41c5dce8b2be52b62511e9266c611
ThibaultYVD <thibaultyvard01@gmail.com>
2024-08-07 11:39:14 +0200

    première verison login

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,27 +1,31 @@
 import axios from 'axios';
+/*
+const instance = axios.create({
+  baseURL: import.meta.env.VUE_APP_DATA_API,
+});
+*/
 
 const instance = axios.create({
-    baseURL: process.env.DATA_API,
-});
+    baseURL: "https://vscode.thibault-yvard.fr/proxy/20241",
+  });
 
 const auth_api = axios.create({
-    baseURL: process.env.AUTH_API,
+  baseURL: import.meta.env.VUE_APP_AUTH_API,
 });
 
-
 instance.interceptors.response.use(response => {
-    return response;
+  return response;
 }, error => {
-    // Vider le stockage local puis redirection pour se reconnecter
-    if (error.response && error.response.status === 401) {
-        clearLocalStorage();
-        window.location.href = '/login';
-    }
-    return Promise.reject(error);
+  // Vider le stockage local puis redirection pour se reconnecter
+  if (error.response && error.response.status === 401) {
+    clearLocalStorage();
+    window.location.href = '/login';
+  }
+  return Promise.reject(error);
 });
 
 function clearLocalStorage() {
-    localStorage.clear();
+  localStorage.clear();
 }
 
-export default {instance, auth_api};
+export { instance, auth_api };
