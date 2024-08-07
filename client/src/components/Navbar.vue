e6da818d5e98650830b7ad0d2a32270c6f8dc4f9
ThibaultYVD <thibaultyvard01@gmail.com>
2024-08-07 16:58:41 +0200

    modifs padding

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -6,8 +6,8 @@
     </button>
     <ul :class="{ open: menuOpen }">
       <li><router-link to="/">Accueil</router-link></li>
-      <li><router-link to="/">A propos</router-link></li>
       <li v-if="!isAuthenticated"><router-link to="/login">Se connecter</router-link></li>
+      <li v-if="!isAuthenticated"><router-link to="/register">S'inscrire</router-link></li>
       <li v-if="isAuthenticated"><router-link to="/dashboard">Dashboard</router-link></li>
       <li v-if="isAuthenticated"><a href="#" @click="logout">Se déconnecter</a></li>
     </ul>
