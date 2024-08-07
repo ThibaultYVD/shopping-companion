fba53bb6d0e07baafe4682d5b3795acc70d9ab4c
THIBAULT YVARD <T.YVARD@groupe-seche.com>
2024-08-07 16:18:58 +0200

    ajout inscription

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -3,11 +3,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import Login from '../views/Login.vue';
 import Dashboard from '../views/Dashboard.vue';
+import Register from '@/views/Register.vue';
 
 const routes = [
   { path: '/', component: Home },
   { path: '/login', component: Login },
+  { path: '/register', component: Register },
   { path: '/dashboard', component: Dashboard, meta: { requiresAuth: true } },
+
 ];
 
 const router = createRouter({
--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,17 +1,21 @@
-<!-- src/views/Login.vue -->
+<!-- src/views/register.vue -->
 <template>
   <div class="background" id="wrapper">
-    <div class="login-container">
+    <div class="register-container">
       <div class="title-container">
-        <h2>Connexion</h2>
+        <h2>Inscription</h2>
       </div>
-      <form @submit.prevent="login">
+      <form @submit.prevent="register">
+
+        <input v-model="first_name" type="text" placeholder="Nom" >
+        <input v-model="last_name" type="text" placeholder="Prénom">
         <input v-model="email" type="email" placeholder="Email">
         <input v-model="password" type="password" placeholder="Mot de passe">
-        <button type="submit">S'identifier</button>
-        <a href="#">Mot de passe oublié ?</a>
-        <div class="register-container">
-          <p>Pas encore inscrit ? <router-link to="/register">S'inscrire</router-link></p>
+        <input v-model="verify_password" type="password" placeholder="Confirmer le mot de passe">
+        <button type="submit">S'inscrire</button>
+
+        <div class="login-container">
+          <p>Déjà inscrit ? <router-link to="/login">Se connecter</router-link></p>
         </div>
       </form>
     </div>
@@ -27,25 +31,30 @@ import { instance as axios } from '../axios';
 
 export default {
   setup() {
+    const first_name = ref('');
+    const last_name = ref('');
     const email = ref('');
     const password = ref('');
     const router = useRouter();
     const authStore = useAuthStore();
 
-    const login = async () => {
+    //console.log(first_name)
+
+    const register = async () => {
       try {
-        const response = await axios.post('/auth/signin', {
+        const value = form.first_name
+        console.log(email.value)
+
+        /*
+        const response = await axios.post('/auth/signup', {
+          first_name: first_name.value,
+          last_name: last_name.value,
           email: email.value,
           password: password.value
         });
-
-        const token = response.data.token;
-        authStore.setToken(token);
-
-        console.log(authStore)
-
-        // Rediriger vers la page Dashboard
-        router.push('/dashboard');
+*/
+        // Rediriger vers la page Login
+        //router.push('/login');
       } catch (error) {
         console.error(error);
         if (error.response.status === 404) {
@@ -58,7 +67,7 @@ export default {
       }
     };
 
-    return { email, password, login };
+    return { email, password, register };
   }
 };
 </script>
@@ -73,11 +82,12 @@ export default {
   background-color: #2C7C45;
 }
 
-.login-container {
+.register-container {
   background-color: #F5F0F6;
   border-radius: 30px;
   box-shadow: rgba(0, 0, 0, 0.50) 0px 0px 37px 0px;
   text-align: center;
+  width: 30%;
 }
 
 .title-container {
@@ -85,7 +95,7 @@ export default {
   justify-content: center;
 }
 
-.login-container h2 {
+.register-container h2 {
   margin-top: 20px;
   margin-bottom: 20px;
   color: #333;
@@ -93,29 +103,30 @@ export default {
   font-size: 25px;
 }
 
-.login-container input {
+.register-container input {
   width: 80%;
   padding: 10px;
   margin: 10px 0;
   border: 1px solid #ccc;
   border-radius: 5px;
   background-color: lightgrey;
-  
+  font-family: Inter;
+
 }
 
-.login-container input::placeholder {
- font-size: 15px;
+.register-container input::placeholder {
+  font-size: 15px;
   font-family: Inter;
 }
 
-.login-container input:focus {
+.register-container input:focus {
   outline: none;
   border-bottom: 3px solid #2C7C45;
   background-color: #F5F0F6;
 
 }
 
-.login-container button {
+.register-container button {
   width: 50%;
   padding: 15px;
   background-color: #F5F0F6;
@@ -128,23 +139,23 @@ export default {
   font-family: Arial, Helvetica, sans-serif;
 }
 
-.login-container button:hover {
+.register-container button:hover {
   background-color: #2C7C45;
   color: #F5F0F6;
 }
 
-.login-container a {
+.register-container a {
   color: #2C7C45;
   text-decoration: none;
   display: block;
   margin-top: 10px;
 }
 
-.login-container a:hover {
+.register-container a:hover {
   text-decoration: underline;
 }
 
-.register-container {
+.login-container {
   background-color: #e0e0e0;
   padding: 10px;
   border-radius: 0 0 30px 30px;
@@ -152,33 +163,47 @@ export default {
 }
 
 
-.register-container p {
+.login-container p {
   font-size: 16px;
   font-weight: 200;
 }
 
 
 
+
+@media (max-width: 1400px) {
+  .register-container {
+    width: 40%;
+  }
+
+}
+
+@media (max-width: 1080px) {
+  .register-container {
+    width: 60%;
+  }
+}
+
 @media (max-width: 768px) {
-  .login-container {
-  background-color: #F5F0F6;
-  border-radius: 30px;
-  text-align: center;
+  .register-container {
+    background-color: #F5F0F6;
+    border-radius: 30px;
+    text-align: center;
 
-  width: 85%;
-}
+    width: 85%;
+  }
 
-.login-container button {
-  width: 50%;
-  padding: 10px;
-  background-color: #F5F0F6;
-  border: solid 3px #2C7C45;
-  border-radius: 10px;
-  cursor: pointer;
-  margin-top: 10px;
-  font-size: 16px;
-  font-weight: 400;
-  font-family: Arial, Helvetica, sans-serif;
-}
+  .register-container button {
+    width: 50%;
+    padding: 10px;
+    background-color: #F5F0F6;
+    border: solid 3px #2C7C45;
+    border-radius: 10px;
+    cursor: pointer;
+    margin-top: 10px;
+    font-size: 16px;
+    font-weight: 400;
+    font-family: Arial, Helvetica, sans-serif;
+  }
 }
 </style>
