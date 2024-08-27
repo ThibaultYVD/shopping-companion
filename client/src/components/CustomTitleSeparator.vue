82afd0600ea270e38d2268b046b8aebdea3adeeb
THIBAULT YVARD <T.YVARD@groupe-seche.com>
2024-08-27 14:30:14 +0200

    refonte du front

---
+++ client/src/components/CustomTitleSeparator.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="custom-title-container">
+        <h1>{{ title }}</h1>
+        <div class="buttons" v-if="buttons.length > 0">
+            <button v-for="(button, index) in buttons" :key="index" @click="button.action" class="action-button">
+                {{ button.label }}
+            </button>
+        </div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CustomTitleSeparator',
+    props: {
+        title: {
+            type: String,
+            required: true,
+        },
+        buttons: {
+            type: Array,
+            default: () => [],
+        },
+    },
+};
+</script>
+
+<style scoped>
+.custom-title-container {
+    padding: 15px 0px 10px 0px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: #2C7C45 solid 5px;
+    border-radius: 0 0px 10px 10px;
+}
+
+.buttons {
+    display: flex;
+    gap: 20px;
+    height: 5vh;
+}
+
+
+.action-button {
+    background-color: white;
+    border: 2px solid #2c7c45;
+    border-radius: 15px;
+    cursor: pointer;
+    font-size: 18px;
+    padding: 10px;
+    font-weight: 400;
+    font-family: Inter;
+}
+
+.action-button:hover {
+    background-color: #2c7c45;
+    color: white
+}
+
+@media (max-width: 1444px) {
+    .custom-title-container {
+        padding: 10px;
+
+    }
+
+    .buttons {
+        gap: 10px;
+        display: flex;
+        justify-content: center;
+
+    }
+
+    .action-button {
+        border-radius: 15px;
+        cursor: pointer;
+        font-size: 16px;
+        padding: 10px;
+        font-weight: 400;
+        font-family: Inter;
+
+    }
+
+}
+
+@media (max-width:768px) {
+    .custom-title-container {
+        display: flex;
+        flex-direction: row;
+    }
+}
+</style>
